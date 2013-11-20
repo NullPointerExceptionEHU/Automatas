ddfcdbbb4df88aa58f6eba1df12dc867e48a9199
acarGo <adriancarenas@gmail.com>
2013-11-20 21:15:46 +0100

    pedirNumVariables y pedirValorK

--- kDNF.hs
+++ kDNF.hs
@@ -1,1 +1,23 @@
-module kDNF where
+module Kdnf where
+import Data.List
+import LibreriaAutomatas
+
+-------------------Comments--------------------
+-- 0 -> False
+-- 1 -> True
+-- 2 -> Negada
+-----------------------------------------------
+
+pedirNumVariables:: IO()
+pedirNumVariables = do putStrLn "Introduce un numero de variables: "
+		       putStr ">> "
+		       numVar <- getLine
+		       if not (isNum numVar) then pedirNumVariables
+		       else putStr "" --Encontrar forma de no estar obligados a utilizar else (Reglas de Haskell)
+		       
+pedirValorK:: IO()
+pedirValorK = do putStrLn "Introduce el valor de k: "
+		 putStr ">> "
+		 numVar <- getLine
+		 if not (isNum numVar) then pedirValorK
+		 else putStr "" --Encontrar forma de no estar obligados a utilizar else (Reglas de Haskell)
